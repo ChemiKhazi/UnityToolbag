10040e8709391709edade7695d3fd622b99037d5
nickgravelyn <nick@gravelyn.com>
2013-12-06 07:55:30 -0800

    Fixed Scriptable Object Utility to use an Editor folder. Made an explicit editor for SpriteFrameAnimation to handle creating the objects.

--- Simple Sprite Animation/SpriteFrameAnimation.cs
+++ Simple Sprite Animation/SpriteFrameAnimation.cs
@@ -4,16 +4,6 @@ using System.Collections;
 public class SpriteFrameAnimation : ScriptableObject
 {
     public bool loop;
-
-    // Duration is in milliseconds
-    public float frameDuration = 15.0f;
+    public float frameDuration = 15.0f; // duration is in milliseconds
     public Sprite[] frames = new Sprite[0];
-
-#if UNITY_EDITOR
-    [UnityEditor.MenuItem("Assets/Create/Sprite Frame Animation")]
-    static void Create()
-    {
-        ScriptableObjectUtility.CreateAsset<SpriteFrameAnimation>();
-    }
-#endif
 }
