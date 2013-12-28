6857323ce9894d1fd86052327bf9c64fc3173c19
nickgravelyn <nick@gravelyn.com>
2013-12-28 09:21:57 -0800

    Adding use of reflection to generate a nicer drop down menu for sorting layers. The editor makes it optional so if the reflection fails due to a change in Unity, the script will fall back to the previous behavior.

--- Sorting Layer Exposed/Editor/SortingLayerExposedEditor.cs
+++ Sorting Layer Exposed/Editor/SortingLayerExposedEditor.cs
@@ -21,39 +21,60 @@
  *    distribution.
  */
 
+using System;
+using System.Reflection;
 using UnityEngine;
 using UnityEditor;
 
-namespace UnityToolbag
-{
+namespace UnityToolbag {
     [CustomEditor(typeof(SortingLayerExposed))]
-    public class SortingLayerExposedEditor : Editor
-    {
-        public override void OnInspectorGUI()
-        {
+    public class SortingLayerExposedEditor : Editor {
+        private string[] _sortingLayerNames;
+
+        void OnEnable()  {
+            var internalEditorUtilityType = Type.GetType("UnityEditorInternal.InternalEditorUtility, UnityEditor");
+            var sortingLayersProperty = internalEditorUtilityType.GetProperty("sortingLayerNames", BindingFlags.Static | BindingFlags.NonPublic);
+            _sortingLayerNames = sortingLayersProperty.GetValue(null, new object[0]) as string[];
+        }
+
+        public override void OnInspectorGUI() {
             // Get the renderer from the target object
             var renderer = (target as SortingLayerExposed).gameObject.renderer;
 
             // If there is no renderer, we can't do anything
-            if (!renderer)
-            {
+            if (!renderer) {
                 return;
             }
 
-            // Expose the sorting layer name
-            string newSortingLayerName = EditorGUILayout.TextField("Sorting Layer Name", renderer.sortingLayerName);
-            if (newSortingLayerName != renderer.sortingLayerName) {
-                Undo.RecordObject(renderer, "Edit Sorting Layer Name");
-                renderer.sortingLayerName = newSortingLayerName;
-                EditorUtility.SetDirty(renderer);
+            // If we have the sorting layers array, we can make a nice dropdown. For stability's sake, if the array is null
+            // we just use our old logic. This makes sure the script works in some fashion even if Unity changes the name of
+            // that internal field we reflected.
+            if (_sortingLayerNames != null) {
+                // Expose drop down for sorting layer
+                int layerIndex = Array.IndexOf(_sortingLayerNames, renderer.sortingLayerName);
+                int newLayerIndex = EditorGUILayout.Popup("Sorting Layer", layerIndex, _sortingLayerNames);
+                if (newLayerIndex != layerIndex) {
+                    Undo.RecordObject(renderer, "Edit Sorting Layer");
+                    renderer.sortingLayerName = _sortingLayerNames[newLayerIndex];
+                    EditorUtility.SetDirty(renderer);
+                }
             }
+            else {
+                // Expose the sorting layer name
+                string newSortingLayerName = EditorGUILayout.TextField("Sorting Layer Name", renderer.sortingLayerName);
+                if (newSortingLayerName != renderer.sortingLayerName) {
+                    Undo.RecordObject(renderer, "Edit Sorting Layer Name");
+                    renderer.sortingLayerName = newSortingLayerName;
+                    EditorUtility.SetDirty(renderer);
+                }
 
-            // Expose the sorting layer ID
-            int newSortingLayerId = EditorGUILayout.IntField("Sorting Layer ID", renderer.sortingLayerID);
-            if (newSortingLayerId != renderer.sortingLayerID) {
-                Undo.RecordObject(renderer, "Edit Sorting Layer ID");
-                renderer.sortingLayerID = newSortingLayerId;
-                EditorUtility.SetDirty(renderer);
+                // Expose the sorting layer ID
+                int newSortingLayerId = EditorGUILayout.IntField("Sorting Layer ID", renderer.sortingLayerID);
+                if (newSortingLayerId != renderer.sortingLayerID) {
+                    Undo.RecordObject(renderer, "Edit Sorting Layer ID");
+                    renderer.sortingLayerID = newSortingLayerId;
+                    EditorUtility.SetDirty(renderer);
+                }
             }
 
             // Expose the manual sorting order
