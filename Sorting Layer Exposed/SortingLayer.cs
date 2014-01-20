85450a0028c2ebd2f4e3a85cbee8b0ebf07c5d4b
Jeiel Aranal <jeiel.aranal@gmail.com>
2014-01-21 02:21:06 +0800

    Adding SortingLayer class and SortingLayerDrawer, exposes sorting layer property to other classes.

---
+++ Sorting Layer Exposed/SortingLayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class SortingLayer {
+
+	public int sortLayer = -1;
+	public string layerName = "";
+	public int layerOrder = 0;
+
+	public void ApplyToRenderer(Renderer targetRenderer)
+	{
+		targetRenderer.sortingLayerID = sortLayer;
+		targetRenderer.sortingOrder = layerOrder;
+		targetRenderer.sortingLayerName = layerName;
+	}
+}
