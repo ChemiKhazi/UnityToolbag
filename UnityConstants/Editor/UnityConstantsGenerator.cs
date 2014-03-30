5b76b71918fb0798b245a8c70b182285ab31394b
nickgravelyn <nick@gravelyn.com>
2014-03-30 00:07:06 -0700

    Deprecating the CodeGenerators in favor of a unified UnityConstantsGenerator that creates a single file with all the constants inside, with a namespace. This is nicer for cases where we may want to continue adding more features so we don't have a ton of files.

--- UnityConstants/Editor/UnityConstantsGenerator.cs
+++ UnityConstants/Editor/UnityConstantsGenerator.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (c) 2014, Nick Gravelyn.
  *
  * This software is provided 'as-is', without any express or implied
@@ -26,13 +26,13 @@ using UnityEditor;
 using System.IO;
 
 namespace UnityToolbag {
-    public static class CodeGenScenes {
-        [MenuItem("UnityToolbag/Code Generators/Scenes")]
+    public static class UnityConstantsGenerator {
+        [MenuItem("Edit/Generate UnityConstants.cs")]
         public static void Generate() {
-            // Try to find an existing file in the project called "Scenes.cs"
+            // Try to find an existing file in the project called "UnityConstants.cs"
             string filePath = string.Empty;
             foreach (var file in Directory.GetFiles(Application.dataPath, "*.cs", SearchOption.AllDirectories)) {
-                if (Path.GetFileNameWithoutExtension(file) == "Scenes") {
+                if (Path.GetFileNameWithoutExtension(file) == "UnityConstants") {
                     filePath = file;
                     break;
                 }
@@ -40,26 +40,58 @@ namespace UnityToolbag {
 
             // If no such file exists already, use the save panel to get a folder in which the file will be placed.
             if (string.IsNullOrEmpty(filePath)) {
-                string directory = EditorUtility.OpenFolderPanel("Choose location for Scenes.cs", Application.dataPath, "");
+                string directory = EditorUtility.OpenFolderPanel("Choose location for UnityConstants.cs", Application.dataPath, "");
 
                 // Canceled choose? Do nothing.
                 if (string.IsNullOrEmpty(directory)) {
                     return;
                 }
 
-                filePath = Path.Combine(directory, "Scenes.cs");
+                filePath = Path.Combine(directory, "UnityConstants.cs");
             }
 
             // Write out our file
             using (var writer = new StreamWriter(filePath)) {
                 writer.WriteLine("// This file is auto-generated. Modifications are not saved.");
                 writer.WriteLine();
+                writer.WriteLine("namespace UnityConstants {");
 
                 // Write out the tags
-                writer.WriteLine("public static class Scenes {");
-                for (int i = 0; i < EditorBuildSettings.scenes.Length; i++) {
-                    writer.WriteLine("    public const int {0} = {1};", MakeSafeForCode(Path.GetFileNameWithoutExtension(EditorBuildSettings.scenes[i].path)), i);
+                writer.WriteLine("    public static class Tags {");
+                foreach (var tag in UnityEditorInternal.InternalEditorUtility.tags) {
+                    writer.WriteLine("        public const string {0} = \"{1}\";", MakeSafeForCode(tag), tag);
                 }
+                writer.WriteLine("    }");
+                writer.WriteLine();
+
+                // Write out sorting layers
+                var sortingLayerNames = SortingLayerHelper.sortingLayerNames;
+                if (sortingLayerNames != null) {
+                    writer.WriteLine("    public static class SortingLayers {");
+                    for (int i = 0; i < sortingLayerNames.Length; i++) {
+                        writer.WriteLine("        public const int {0} = {1};", MakeSafeForCode(sortingLayerNames[i]), i);
+                    }
+                    writer.WriteLine("    }");
+                    writer.WriteLine();
+                }
+
+                // Write out layers
+                writer.WriteLine("    public static class Layers {");
+                for (int i = 0; i < 32; i++) {
+                    string layer = UnityEditorInternal.InternalEditorUtility.GetLayerName(i);
+                    if (!string.IsNullOrEmpty(layer)) {
+                        writer.WriteLine("        public const int {0} = {1};", MakeSafeForCode(layer), i);
+                    }
+                }
+                writer.WriteLine("    }");
+                writer.WriteLine();
+
+                // Write out scenes
+                writer.WriteLine("    public static class Scenes {");
+                for (int i = 0; i < EditorBuildSettings.scenes.Length; i++) {
+                    writer.WriteLine("        public const int {0} = {1};", MakeSafeForCode(Path.GetFileNameWithoutExtension(EditorBuildSettings.scenes[i].path)), i);
+                }
+                writer.WriteLine("    }");
                 writer.WriteLine("}");
                 writer.WriteLine();
             }
