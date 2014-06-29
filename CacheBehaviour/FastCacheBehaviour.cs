d8c4e0a8d8d956cd6c1bbf7c4a94c52504796c97
Nick Gravelyn <nick@gravelyn.com>
2014-06-28 18:24:05 -0700

    BREAKING CHANGE to FastCacheBehaviour.
    Looks like the previous use of ReferenceEquals wasn't always working. I was getting null errors when trying to use this in my game. So in the effort of fastest speed with little safety, I've gone a new route to remove properties from FastCacheBehaviour entirely! Instead they're just plain fields which makes them even faster to access, but now you either have to call the Reset*Cache method for the specific component you want, the ResetCache() method to get all the components, or ResetCacheDestroyed() to get all components that are no longer valid. Or you can manually assign them now that they're just plain fields.
    
    This will break existing usages but makes the class even more performant and simplified.

--- CacheBehaviour/FastCacheBehaviour.cs
+++ CacheBehaviour/FastCacheBehaviour.cs
@@ -26,74 +26,315 @@ using System;
 
 namespace UnityToolbag
 {
+    /// <summary>
+    /// A replacement for MonoBehaviour as your base class that stores fields for all common components that are normally properties
+    /// on MonoBehaviour. You must use one of the many Reset methods to get the components you want, or manually assign the values yourself.
+    /// However this allows for the fastest access to the components since they are just plain fields with no property call or GetComponent
+    /// overhead.
+    /// </summary>
     public abstract class FastCacheBehaviour : MonoBehaviour
     {
+        /// <summary>
+        /// The cached Animation component attached to the object.
+        /// Will be null if you haven't called ResetAnimationCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
         [HideInInspector]
-        private Animation _animation;
-        public new Animation animation { get { return ReferenceEquals(_animation, null) ? (_animation = GetComponent<Animation>()) : _animation; } }
+        public new Animation animation;
 
-        [HideInInspector]
-        private AudioSource _audio;
-        public new AudioSource audio { get { return ReferenceEquals(_audio, null) ? (_audio = GetComponent<AudioSource>()) : _audio; } }
+        /// <summary>
+        /// Caches the current Animation attached to the object.
+        /// </summary>
+        public void ResetAnimationCache() { animation = GetComponent<Animation>(); }
 
+        /// <summary>
+        /// The cached AudioSource component attached to the object.
+        /// Will be null if you haven't called ResetAudioSourceCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
         [HideInInspector]
-        private Camera _camera;
-        public new Camera camera { get { return ReferenceEquals(_camera, null) ? (_camera = GetComponent<Camera>()) : _camera; } }
+        public new AudioSource audio;
 
-        [HideInInspector]
-        private Collider _collider;
-        public new Collider collider { get { return ReferenceEquals(_collider, null) ? (_collider = GetComponent<Collider>()) : _collider; } }
+        /// <summary>
+        /// Caches the current AudioSource attached to the object.
+        /// </summary>
+        public void ResetAudioSourceCache() { audio = GetComponent<AudioSource>(); }
 
+        /// <summary>
+        /// The cached Camera component attached to the object.
+        /// Will be null if you haven't called ResetCameraCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
         [HideInInspector]
-        private Collider2D _collider2D;
-        public new Collider2D collider2D { get { return ReferenceEquals(_collider2D, null) ? (_collider2D = GetComponent<Collider2D>()) : _collider2D; } }
+        public new Camera camera;
 
-        [HideInInspector]
-        private ConstantForce _constantForce;
-        public new ConstantForce constantForce { get { return ReferenceEquals(_constantForce, null) ? (_constantForce = GetComponent<ConstantForce>()) : _constantForce; } }
+        /// <summary>
+        /// Caches the current Camera attached to the object.
+        /// </summary>
+        public void ResetCameraCache() { camera = GetComponent<Camera>(); }
 
+        /// <summary>
+        /// The cached Collider component attached to the object.
+        /// Will be null if you haven't called ResetColliderCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
         [HideInInspector]
-        private GUIText _guiText;
-        public new GUIText guiText { get { return ReferenceEquals(_guiText, null) ? (_guiText = GetComponent<GUIText>()) : _guiText; } }
+        public new Collider collider;
 
-        [HideInInspector]
-        private GUITexture _guiTexture;
-        public new GUITexture guiTexture { get { return ReferenceEquals(_guiTexture, null) ? (_guiTexture = GetComponent<GUITexture>()) : _guiTexture; } }
+        /// <summary>
+        /// Caches the current Collider attached to the object.
+        /// </summary>
+        public void ResetColliderCache() { collider = GetComponent<Collider>(); }
 
+        /// <summary>
+        /// The cached Collider2D component attached to the object.
+        /// Will be null if you haven't called ResetCollider2DCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
         [HideInInspector]
-        private HingeJoint _hingeJoint;
-        public new HingeJoint hingeJoint { get { return ReferenceEquals(_hingeJoint, null) ? (_hingeJoint = GetComponent<HingeJoint>()) : _hingeJoint; } }
+        public new Collider2D collider2D;
 
-        [HideInInspector]
-        private Light _light;
-        public new Light light { get { return ReferenceEquals(_light, null) ? (_light = GetComponent<Light>()) : _light; } }
+        /// <summary>
+        /// Caches the current Collider2D attached to the object.
+        /// </summary>
+        public void ResetCollider2DCache() { collider2D = GetComponent<Collider2D>(); }
 
+        /// <summary>
+        /// The cached ConstantForce component attached to the object.
+        /// Will be null if you haven't called ResetConstantForceCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
         [HideInInspector]
-        private NetworkView _networkView;
-        public new NetworkView networkView { get { return ReferenceEquals(_networkView, null) ? (_networkView = GetComponent<NetworkView>()) : _networkView; } }
+        public new ConstantForce constantForce;
 
-        [HideInInspector]
-        private ParticleEmitter _particleEmitter;
-        public new ParticleEmitter particleEmitter { get { return ReferenceEquals(_particleEmitter, null) ? (_particleEmitter = GetComponent<ParticleEmitter>()) : _particleEmitter; } }
+        /// <summary>
+        /// Caches the current ConstantForce attached to the object.
+        /// </summary>
+        public void ResetConstantForceCache() { constantForce = GetComponent<ConstantForce>(); }
 
+        /// <summary>
+        /// The cached GUIText component attached to the object.
+        /// Will be null if you haven't called ResetGUITextCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
         [HideInInspector]
-        private ParticleSystem _particleSystem;
-        public new ParticleSystem particleSystem { get { return ReferenceEquals(_particleSystem, null) ? (_particleSystem = GetComponent<ParticleSystem>()) : _particleSystem; } }
+        public new GUIText guiText;
 
-        [HideInInspector]
-        private Renderer _renderer;
-        public new Renderer renderer { get { return ReferenceEquals(_renderer, null) ? (_renderer = GetComponent<Renderer>()) : _renderer; } }
+        /// <summary>
+        /// Caches the current GUIText attached to the object.
+        /// </summary>
+        public void ResetGUITextCache() { guiText = GetComponent<GUIText>(); }
 
+        /// <summary>
+        /// The cached GUITexture component attached to the object.
+        /// Will be null if you haven't called ResetGUITextureCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
         [HideInInspector]
-        private Rigidbody _rigidbody;
-        public new Rigidbody rigidbody { get { return ReferenceEquals(_rigidbody, null) ? (_rigidbody = GetComponent<Rigidbody>()) : _rigidbody; } }
+        public new GUITexture guiTexture;
 
-        [HideInInspector]
-        private Rigidbody2D _rigidbody2D;
-        public new Rigidbody2D rigidbody2D { get { return ReferenceEquals(_rigidbody2D, null) ? (_rigidbody2D = GetComponent<Rigidbody2D>()) : _rigidbody2D; } }
+        /// <summary>
+        /// Caches the current GUITexture attached to the object.
+        /// </summary>
+        public void ResetGUITextureCache() { guiTexture = GetComponent<GUITexture>(); }
 
+        /// <summary>
+        /// The cached HingeJoint component attached to the object.
+        /// Will be null if you haven't called ResetHingeJointCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
         [HideInInspector]
-        private Transform _transform;
-        public new Transform transform { get { return ReferenceEquals(_transform, null) ? (_transform = GetComponent<Transform>()) : _transform; } }
+        public new HingeJoint hingeJoint;
+
+        /// <summary>
+        /// Caches the current HingeJoint attached to the object.
+        /// </summary>
+        public void ResetHingeJointCache() { hingeJoint = GetComponent<HingeJoint>(); }
+
+        /// <summary>
+        /// The cached Light component attached to the object.
+        /// Will be null if you haven't called ResetLightCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
+        [HideInInspector]
+        public new Light light;
+
+        /// <summary>
+        /// Caches the current Light attached to the object.
+        /// </summary>
+        public void ResetLightCache() { light = GetComponent<Light>(); }
+
+        /// <summary>
+        /// The cached NetworkView component attached to the object.
+        /// Will be null if you haven't called ResetNetworkViewCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
+        [HideInInspector]
+        public new NetworkView networkView;
+
+        /// <summary>
+        /// Caches the current NetworkView attached to the object.
+        /// </summary>
+        public void ResetNetworkViewCache() { networkView = GetComponent<NetworkView>(); }
+
+        /// <summary>
+        /// The cached ParticleEmitter component attached to the object.
+        /// Will be null if you haven't called ResetParticleEmitterCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
+        [HideInInspector]
+        public new ParticleEmitter particleEmitter;
+
+        /// <summary>
+        /// Caches the current ParticleEmitter attached to the object.
+        /// </summary>
+        public void ResetParticleEmitterCache() { particleEmitter = GetComponent<ParticleEmitter>(); }
+
+        /// <summary>
+        /// The cached ParticleSystem component attached to the object.
+        /// Will be null if you haven't called ResetParticleSystemCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
+        [HideInInspector]
+        public new ParticleSystem particleSystem;
+
+        /// <summary>
+        /// Caches the current ParticleSystem attached to the object.
+        /// </summary>
+        public void ResetParticleSystemCache() { particleSystem = GetComponent<ParticleSystem>(); }
+
+        /// <summary>
+        /// The cached Renderer component attached to the object.
+        /// Will be null if you haven't called ResetRendererCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
+        [HideInInspector]
+        public new Renderer renderer;
+
+        /// <summary>
+        /// Caches the current Renderer attached to the object.
+        /// </summary>
+        public void ResetRendererCache() { renderer = GetComponent<Renderer>(); }
+
+        /// <summary>
+        /// The cached Rigidbody component attached to the object.
+        /// Will be null if you haven't called ResetRigidbodyCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
+        [HideInInspector]
+        public new Rigidbody rigidbody;
+
+        /// <summary>
+        /// Caches the current Rigidbody attached to the object.
+        /// </summary>
+        public void ResetRigidbodyCache() { rigidbody = GetComponent<Rigidbody>(); }
+
+        /// <summary>
+        /// The cached Rigidbody2D component attached to the object.
+        /// Will be null if you haven't called ResetRigidbody2DCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
+        [HideInInspector]
+        public new Rigidbody2D rigidbody2D;
+
+        /// <summary>
+        /// Caches the current Rigidbody2D attached to the object.
+        /// </summary>
+        public void ResetRigidbody2DCache() { rigidbody2D = GetComponent<Rigidbody2D>(); }
+
+        /// <summary>
+        /// The cached Transform component attached to the object.
+        /// Will be null if you haven't called ResetTransformCache(), ResetCache(), ResetCacheDestroyed(), or manually stored the component yourself.
+        /// </summary>
+        [HideInInspector]
+        public new Transform transform;
+
+        /// <summary>
+        /// Caches the current Transform attached to the object.
+        /// </summary>
+        public void ResetTransformCache() { transform = GetComponent<Transform>(); }
+
+        /// <summary>
+        /// Caches all of the current standard components attached to the object.
+        /// </summary>
+        public void ResetCache()
+        {
+            animation = GetComponent<Animation>();
+            audio = GetComponent<AudioSource>();
+            camera = GetComponent<Camera>();
+            collider = GetComponent<Collider>();
+            collider2D = GetComponent<Collider2D>();
+            constantForce = GetComponent<ConstantForce>();
+            guiText = GetComponent<GUIText>();
+            guiTexture = GetComponent<GUITexture>();
+            hingeJoint = GetComponent<HingeJoint>();
+            light = GetComponent<Light>();
+            networkView = GetComponent<NetworkView>();
+            particleEmitter = GetComponent<ParticleEmitter>();
+            particleSystem = GetComponent<ParticleSystem>();
+            renderer = GetComponent<Renderer>();
+            rigidbody = GetComponent<Rigidbody>();
+            rigidbody2D = GetComponent<Rigidbody2D>();
+            transform = GetComponent<Transform>();
+        }
+
+        /// <summary>
+        /// Calls GetComponent for all cached components if the currently cached instance is no longer valid.
+        /// </summary>
+        public void ResetCacheDestroyed()
+        {
+            if (!animation)
+            {
+                animation = GetComponent<Animation>();
+            }
+            if (!audio)
+            {
+                audio = GetComponent<AudioSource>();
+            }
+            if (!camera)
+            {
+                camera = GetComponent<Camera>();
+            }
+            if (!collider)
+            {
+                collider = GetComponent<Collider>();
+            }
+            if (!collider2D)
+            {
+                collider2D = GetComponent<Collider2D>();
+            }
+            if (!constantForce)
+            {
+                constantForce = GetComponent<ConstantForce>();
+            }
+            if (!guiText)
+            {
+                guiText = GetComponent<GUIText>();
+            }
+            if (!guiTexture)
+            {
+                guiTexture = GetComponent<GUITexture>();
+            }
+            if (!hingeJoint)
+            {
+                hingeJoint = GetComponent<HingeJoint>();
+            }
+            if (!light)
+            {
+                light = GetComponent<Light>();
+            }
+            if (!networkView)
+            {
+                networkView = GetComponent<NetworkView>();
+            }
+            if (!particleEmitter)
+            {
+                particleEmitter = GetComponent<ParticleEmitter>();
+            }
+            if (!particleSystem)
+            {
+                particleSystem = GetComponent<ParticleSystem>();
+            }
+            if (!renderer)
+            {
+                renderer = GetComponent<Renderer>();
+            }
+            if (!rigidbody)
+            {
+                rigidbody = GetComponent<Rigidbody>();
+            }
+            if (!rigidbody2D)
+            {
+                rigidbody2D = GetComponent<Rigidbody2D>();
+            }
+            if (!transform)
+            {
+                transform = GetComponent<Transform>();
+            }
+        }
     }
 }
--- CacheBehaviour/README.md
+++ CacheBehaviour/README.md
@@ -6,8 +6,10 @@ There are two reasons not to use the default component properties (e.g. `rigidBo
 1. The properties do no caching, so each time you use the property you are calling `GetComponent`.
 2. Unity 5 is [removing all of them](http://blogs.unity3d.com/2014/06/23/unity5-api-changes-automatic-script-updating/) aside from the `transform` property (which it will start caching).
 
-The two scripts in this folder act as replacements as script base classes instead of using `MonoBehaviour` directly. These provide all the same properties as `MonoBehaviour` does currently, but they cache the results to optimize the properties.
+The two scripts in this folder act as replacements as script base classes instead of using `MonoBehaviour` directly, but each behave differently.
 
-The two scripts differ in one big way: how they test for the cached value. Unity does some [clever things](http://blogs.unity3d.com/2014/05/16/custom-operator-should-we-keep-it/) when you compare components since the managed objects are just wrappers around the native objects. With this in mind, `CacheBehaviour` is a "safe" cache in that it performs the implicit cast to bool which will check the components for being non-null and not destroyed. `FastCacheBehaviour` does a direct null reference check that is much faster, but doesn't check if the object is not destroyed.
+`CacheBehaviour` is the safer of the two and is a drop-in replacement for `MonoBehaviour`. The properties will check the cache variable and call `GetComponent` anytime the cached value is invalid.
 
-`CacheBehaviour` is the safer of the two options since it can handle components being destroyed and re-added, however `FastCacheBehaviour` has the better performance for situations where you are not worried about components being destroyed during the lifetime of the script. Since both expose the same properties, you can easily swap between them as desired or needed while working.
+`FastCacheBehaviour` uses fields instead of properties. This makes accessing the components much faster but means you have to do some work to retrieve the values. The easiest solution is to call `ResetCache()` in the `Start()` or `OnEnable()` methods of your subclass, but you can also use the component specific methods (e.g. `ResetRigidbodyCache()`) or just manually call `GetComponent` yourself and assign the value. This adds some extra work, but then the cached values are just plain fields, with the quickest access possible.
+
+If you're not calling into a lot of the components each frame, `CacheBehaviour` is the simpler of the two, but if you're accessing a lot of components frequently, `FastCacheBehaviour` might be the better way to go.
