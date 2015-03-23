579e018d3e726f5dfb70d9fa5cfc0c3f56914caf
Nick Gravelyn <nick@gravelyn.com>
2015-03-23 09:00:40 -0700

    Updating DrawTitleSafeArea:
    - Colors are now serialized fields for customization
    - Title safe margins are now fields for customization
    - Added enum for different size modes (percentage or pixels)
    - Bit of code cleanup

--- DrawTitleSafeArea/DrawTitleSafeArea.cs
+++ DrawTitleSafeArea/DrawTitleSafeArea.cs
@@ -2,23 +2,54 @@
 
 namespace UnityToolbag
 {
+    public enum TitleSafeSizeMode
+    {
+        Pixels,
+        Percentage
+    }
+
     [ExecuteInEditMode]
     [AddComponentMenu("UnityToolbag/Draw Title Safe Area")]
     public class DrawTitleSafeArea : MonoBehaviour
     {
-    #if UNITY_EDITOR
-        private static Texture2D _blank;
-    #endif
+#if UNITY_EDITOR
+        private Texture2D _blank;
+#endif
 
-        public float alpha = .15f;
-        public new Camera camera;
+        [SerializeField]
+        private Color _innerColor = new Color(1, 1, 0, 0.15f);
 
-    #if UNITY_EDITOR
-        void Start()
+        [SerializeField]
+        private Color _outerColor = new Color(1, 0, 0, 0.15f);
+
+        [SerializeField]
+        private TitleSafeSizeMode _sizeMode = TitleSafeSizeMode.Percentage;
+
+        [SerializeField]
+        private int _sizeX = 5;
+
+        [SerializeField]
+        private int _sizeY = 5;
+
+#if UNITY_EDITOR
+        void OnValidate()
         {
-            if (!this.camera)
-            {
-                this.camera = base.GetComponent<Camera>();
+            if (_sizeX < 0) {
+                _sizeX = 0;
+            }
+
+            if (_sizeY < 0) {
+                _sizeY = 0;
+            }
+
+            if (_sizeMode == TitleSafeSizeMode.Percentage) {
+                if (_sizeX > 25) {
+                    _sizeX = 25;
+                }
+
+                if (_sizeY > 25) {
+                    _sizeY = 25;
+                }
             }
         }
 
@@ -33,13 +64,12 @@ namespace UnityToolbag
 
         void OnGUI()
         {
-            if (!this.camera)
-            {
+            var camera = GetComponent<Camera>();
+            if (!camera) {
                 return;
             }
 
-            if (!_blank)
-            {
+            if (!_blank) {
                 _blank = new Texture2D(1, 1);
                 _blank.SetPixel(0, 0, Color.white);
                 _blank.hideFlags = HideFlags.HideAndDontSave;
@@ -48,24 +78,37 @@ namespace UnityToolbag
             float w = camera.pixelWidth;
             float h = camera.pixelHeight;
 
-            // .05 = 5% for each side = 10% total safe area
-            float wMargin = w * .05f;
-            float hMargin = h * .05f;
+            // Compute the actual sizes based on the size mode and our sizes
+            float wMargin = 0;
+            float hMargin = 0;
+            switch (_sizeMode) {
+                case TitleSafeSizeMode.Percentage: {
+                    wMargin = w * (_sizeX / 100.0f);
+                    hMargin = h * (_sizeY / 100.0f);
+                    break;
+                }
+                case TitleSafeSizeMode.Pixels: {
+                    // Clamp to 1/4 the screen size so we never overlap the other side
+                    wMargin = Mathf.Clamp(_sizeX, 0, w / 4);
+                    hMargin = Mathf.Clamp(_sizeY, 0, h / 4);
+                    break;
+                }
+            }
 
-            // red in the outer 10% is drawn because all the action of your game should be inside
-            GUI.color = new Color(1, 0, 0, alpha);
-            GUI.DrawTexture(new Rect(0, 0, w, hMargin), _blank, ScaleMode.StretchToFill, true);
-            GUI.DrawTexture(new Rect(0, h - hMargin, w, hMargin), _blank, ScaleMode.StretchToFill, true);
-            GUI.DrawTexture(new Rect(0, hMargin, wMargin, h - hMargin * 2), _blank, ScaleMode.StretchToFill, true);
-            GUI.DrawTexture(new Rect(w - wMargin, hMargin, wMargin, h - hMargin * 2), _blank, ScaleMode.StretchToFill, true);
+            // Draw the outer region first
+            GUI.color = _outerColor;
+            GUI.DrawTexture(new Rect(0,           0,           w,       hMargin),         _blank, ScaleMode.StretchToFill, true);
+            GUI.DrawTexture(new Rect(0,           h - hMargin, w,       hMargin),         _blank, ScaleMode.StretchToFill, true);
+            GUI.DrawTexture(new Rect(0,           hMargin,     wMargin, h - hMargin * 2), _blank, ScaleMode.StretchToFill, true);
+            GUI.DrawTexture(new Rect(w - wMargin, hMargin,     wMargin, h - hMargin * 2), _blank, ScaleMode.StretchToFill, true);
 
-            // yellow in the outer 10-20% range is drawn because all critical UI and text should be inside
-            GUI.color = new Color(1, 1, 0, alpha);
-            GUI.DrawTexture(new Rect(wMargin, hMargin, w - wMargin * 2, hMargin), _blank, ScaleMode.StretchToFill, true);
-            GUI.DrawTexture(new Rect(wMargin, h - hMargin * 2, w - wMargin * 2, hMargin), _blank, ScaleMode.StretchToFill, true);
-            GUI.DrawTexture(new Rect(wMargin, hMargin * 2, wMargin, h - hMargin * 4), _blank, ScaleMode.StretchToFill, true);
-            GUI.DrawTexture(new Rect(w - wMargin * 2, hMargin * 2, wMargin, h - hMargin * 4), _blank, ScaleMode.StretchToFill, true);
+            // Then the inner region
+            GUI.color = _innerColor;
+            GUI.DrawTexture(new Rect(wMargin,         hMargin,         w - wMargin * 2, hMargin),         _blank, ScaleMode.StretchToFill, true);
+            GUI.DrawTexture(new Rect(wMargin,         h - hMargin * 2, w - wMargin * 2, hMargin),         _blank, ScaleMode.StretchToFill, true);
+            GUI.DrawTexture(new Rect(wMargin,         hMargin * 2,     wMargin,         h - hMargin * 4), _blank, ScaleMode.StretchToFill, true);
+            GUI.DrawTexture(new Rect(w - wMargin * 2, hMargin * 2,     wMargin,         h - hMargin * 4), _blank, ScaleMode.StretchToFill, true);
         }
-    #endif // UNITY_EDITOR
+#endif // UNITY_EDITOR
     }
 }
