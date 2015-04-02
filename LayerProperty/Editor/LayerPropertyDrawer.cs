5a04bcf7d650b3b9d7c9897de8681194314668b2
Jeiel <jeiel.aranal@gmail.com>
2015-04-03 05:14:36 +0800

    Add Unity layer property drawer.

---
+++ LayerProperty/Editor/LayerPropertyDrawer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace UnityToolbag
+{
+    [CustomPropertyDrawer(typeof(LayerProperty))]
+    public class LayerPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.Integer) {
+				EditorGUI.HelpBox(position, string.Format("{0} is not an integer but has [LayerProperty].", property.name), MessageType.Error);
+            }
+            else
+            {
+	            List<string> layerNames = new List<string>();
+	            List<int> layerIndices = new List<int>();
+	            for (int i = 0; i < 32; i++)
+	            {
+		            string layer = UnityEditorInternal.InternalEditorUtility.GetLayerName(i);
+		            if (!string.IsNullOrEmpty(layer))
+		            {
+						layerNames.Add(layer);
+			            layerIndices.Add(i);
+		            }
+	            }
+	            int currentSelection = layerIndices.IndexOf(property.intValue);
+	            if (currentSelection < 0)
+		            currentSelection = 0;
+
+	            EditorGUI.BeginProperty(position, label, property);
+	            int newSelection = EditorGUI.Popup(position, label.text, currentSelection, layerNames.ToArray());
+				if (newSelection != currentSelection)
+				{
+					property.intValue = layerIndices[newSelection];
+				}
+                EditorGUI.EndProperty();
+            }
+        }
+    }
+}
---
+++ LayerProperty/LayerProperty.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UnityToolbag
+{
+	/// <summary>
+	/// Mark an integer field as a Unity layer, so it displays as a dropdown in the Inspector
+	/// </summary>
+    public class LayerProperty : PropertyAttribute
+    {
+    }
+}
