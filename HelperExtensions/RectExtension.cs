43bd224e73d19881f992d6fd1c84ff4acfbe0677
Jeiel <jeiel.aranal@gmail.com>
2015-04-15 16:01:32 +0800

    Added Rect extensions

---
+++ HelperExtensions/RectExtension.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class RectExtension
+{
+
+	public static Rect ScreenAlign(this Rect input, SpriteAlignment align)
+	{
+		Vector2 size = new Vector2(input.width, input.height);
+
+		// X align
+		if (align == SpriteAlignment.TopLeft ||
+			align == SpriteAlignment.LeftCenter ||
+			align == SpriteAlignment.BottomLeft)
+		{
+			input.xMin = input.xMin;
+		}
+
+		if (align == SpriteAlignment.TopCenter ||
+			align == SpriteAlignment.Center ||
+			align == SpriteAlignment.BottomCenter)
+		{
+			input.xMin = (Screen.width / 2) + input.xMin;
+		}
+
+		if (align == SpriteAlignment.TopRight ||
+			align == SpriteAlignment.RightCenter ||
+			align == SpriteAlignment.BottomRight)
+		{
+			input.xMin = (Screen.width) - input.xMin;
+		}
+
+		// Y align
+		if (align == SpriteAlignment.TopLeft ||
+			align == SpriteAlignment.TopLeft ||
+			align == SpriteAlignment.TopRight)
+		{
+			input.yMin = input.yMin;
+		}
+		if (align == SpriteAlignment.LeftCenter ||
+			align == SpriteAlignment.Center ||
+			align == SpriteAlignment.RightCenter)
+		{
+			input.yMin = (Screen.height / 2) + input.yMin;
+		}
+		if (align == SpriteAlignment.BottomLeft ||
+			align == SpriteAlignment.BottomCenter ||
+			align == SpriteAlignment.BottomRight)
+		{
+			input.yMin = (Screen.height) - input.yMin;
+		}
+
+		input.width = size.x;
+		input.height = size.y;
+		return input;
+	}
+}
