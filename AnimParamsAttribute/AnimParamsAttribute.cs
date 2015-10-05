31faa293bcbca0fbad7e3675e383a0e95558c909
Jeiel Aranal <jeiel.aranal@gmail.com>
2015-10-05 21:44:07 +0800

    Modified AnimParamsAttribute so it can pull animation parameters from objects outside the MonoBehaviour it is used in.

--- AnimParamsAttribute/AnimParamsAttribute.cs
+++ AnimParamsAttribute/AnimParamsAttribute.cs
@@ -1,14 +1,50 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace UnityToolbag
 {
     public class AnimParamsAttribute : PropertyAttribute
     {
-        public string animatorProp;
+        public string animPath;
+        public bool isConfig;
 
+		/// <summary>
+		/// Show the parameters of an Animator as a drop down in the inspector
+		/// </summary>
+		/// <param name="animatorPropName">The field name of an Animator component in the same MonoBehaviour</param>
         public AnimParamsAttribute(string animatorPropName)
         {
-            animatorProp = animatorPropName;
+            animPath = animatorPropName;
+	        isConfig = false;
         }
+
+		/// <summary>
+		/// Show the parameters of an Animator as a drop down in the inspector
+		/// </summary>
+		/// <param name="configPath">The field name of the Animator or AnimParamsConfig in this MonoBehaviour</param>
+		/// <param name="isConfig">If true, configPath points to an AnimParamsConfig</param>
+        public AnimParamsAttribute(string configPath, bool isConfig)
+        {
+			animPath = configPath;
+			this.isConfig = isConfig;
+        }
+    }
+
+	/// <summary>
+	/// Inspector configurator for AnimParamsAttribute
+	/// </summary>
+    [Serializable]
+    public class AnimParamsConfig
+    {
+        public enum RefType
+        {
+            Animator,
+            Controller
+        }
+
+		[SerializeField]
+        protected RefType referenceType;
+		[SerializeField]
+        protected string instanceId;
     }
 }
