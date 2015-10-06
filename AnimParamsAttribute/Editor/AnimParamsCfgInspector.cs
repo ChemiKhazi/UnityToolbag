877827c9cf47bd083b4b3f228d2b6ef14632ab3a
Jeiel Aranal <jeiel.aranal@gmail.com>
2015-10-06 10:27:09 +0800

    Actually add the AnimParamsCfgInspector

---
+++ AnimParamsAttribute/Editor/AnimParamsCfgInspector.cs
@@ -0,0 +1,57 @@
+﻿using Mono.Cecil;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace UnityToolbag
+{
+	[CustomPropertyDrawer(typeof(AnimParamsConfig))]
+	public class AnimParamsCfgInspector : PropertyDrawer
+	{
+		void OnEnable()
+		{
+		}
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			Rect rectItem = new Rect(position);
+			rectItem.height = EditorGUIUtility.singleLineHeight;
+
+			property.isExpanded = EditorGUI.Foldout(rectItem, property.isExpanded, label);
+
+			if (!property.isExpanded)
+				return;
+
+			EditorGUI.indentLevel++;
+
+			rectItem.yMin = rectItem.yMax;
+			rectItem.yMax = rectItem.yMin + EditorGUIUtility.singleLineHeight;
+			SerializedProperty pathProp = property.FindPropertyRelative("instanceId");
+
+			AnimatorController prevCtrl = null;
+			int ctrlId;
+			if (int.TryParse(pathProp.stringValue, out ctrlId))
+			{
+				prevCtrl = EditorUtility.InstanceIDToObject(ctrlId) as AnimatorController;
+			}
+
+			AnimatorController newCtrl = EditorGUI.ObjectField(rectItem,
+										new GUIContent("Animator Controller"), prevCtrl,
+										typeof(AnimatorController), false) as AnimatorController;
+
+			if (prevCtrl != newCtrl)
+			{
+				string newId = (newCtrl != null) ? newCtrl.GetInstanceID().ToString() : "";
+				pathProp.stringValue = newId;
+				property.serializedObject.ApplyModifiedProperties();
+			}
+
+			EditorGUI.indentLevel--;
+		}
+
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			return EditorGUI.GetPropertyHeight(property, label);
+		}
+	}
+}
