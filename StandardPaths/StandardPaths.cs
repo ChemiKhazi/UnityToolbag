92acfe6a40c07419e733f849050df0db230fadb2
Nick Gravelyn <nick@gravelyn.com>
2015-11-29 14:05:24 -0800

    Forgot my own namespace on StandardPaths

--- StandardPaths/StandardPaths.cs
+++ StandardPaths/StandardPaths.cs
@@ -2,215 +2,218 @@
 using System.IO;
 using UnityEngine;
 
-public static class StandardPaths
+namespace UnityToolbag
 {
-    public static bool includeCompanyName { get; set; }
-
-    public static string saveDataDirectory
+    public static class StandardPaths
     {
-        get
-        {
-            string path;
+        public static bool includeCompanyName { get; set; }
 
-            switch (Application.platform) {
-                case RuntimePlatform.WindowsEditor:
-                case RuntimePlatform.WindowsPlayer:
-                {
-                    path = Windows.GetPath("Saves");
-                    break;
+        public static string saveDataDirectory
+        {
+            get
+            {
+                string path;
+
+                switch (Application.platform) {
+                    case RuntimePlatform.WindowsEditor:
+                    case RuntimePlatform.WindowsPlayer:
+                    {
+                        path = Windows.GetPath("Saves");
+                        break;
+                    }
+                    case RuntimePlatform.OSXEditor:
+                    case RuntimePlatform.OSXPlayer:
+                    {
+                        path = OSX.GetApplicationSupportPath("Saves");
+                        break;
+                    }
+                    case RuntimePlatform.LinuxPlayer:
+                    {
+                        path = Linux.GetSaveDirectory();
+                        break;
+                    }
+                    default:
+                    {
+                        path = Application.persistentDataPath;
+                        break;
+                    }
                 }
-                case RuntimePlatform.OSXEditor:
-                case RuntimePlatform.OSXPlayer:
-                {
-                    path = OSX.GetApplicationSupportPath("Saves");
-                    break;
+
+                Directory.CreateDirectory(path);
+                return path;
+            }
+        }
+
+        public static string configDirectory
+        {
+            get
+            {
+                string path;
+
+                switch (Application.platform) {
+                    case RuntimePlatform.WindowsEditor:
+                    case RuntimePlatform.WindowsPlayer:
+                    {
+                        path = Windows.GetPath("Config");
+                        break;
+                    }
+                    case RuntimePlatform.OSXEditor:
+                    case RuntimePlatform.OSXPlayer:
+                    {
+                        path = OSX.GetApplicationSupportPath("Config");
+                        break;
+                    }
+                    case RuntimePlatform.LinuxPlayer:
+                    {
+                        path = Linux.GetConfigDirectory();
+                        break;
+                    }
+                    default:
+                    {
+                        path = Application.persistentDataPath;
+                        break;
+                    }
                 }
-                case RuntimePlatform.LinuxPlayer:
-                {
-                    path = Linux.GetSaveDirectory();
-                    break;
+
+                Directory.CreateDirectory(path);
+                return path;
+            }
+        }
+
+        public static string logDirectory
+        {
+            get
+            {
+                string path;
+
+                switch (Application.platform) {
+                    case RuntimePlatform.WindowsEditor:
+                    case RuntimePlatform.WindowsPlayer:
+                    {
+                        path = Windows.GetPath("Logs");
+                        break;
+                    }
+                    case RuntimePlatform.OSXEditor:
+                    case RuntimePlatform.OSXPlayer:
+                    {
+                        path = OSX.GetLogsPath();
+                        break;
+                    }
+                    case RuntimePlatform.LinuxPlayer:
+                    {
+                        path = Linux.GetLogDirectory();
+                        break;
+                    }
+                    default:
+                    {
+                        path = Application.persistentDataPath;
+                        break;
+                    }
                 }
-                default:
-                {
-                    path = Application.persistentDataPath;
-                    break;
-                }
+
+                Directory.CreateDirectory(path);
+                return path;
+            }
+        }
+
+        static StandardPaths()
+        {
+            includeCompanyName = false;
+        }
+
+        private static string AppendProductPath(string path)
+        {
+            if (includeCompanyName) {
+                path = AppendDirectory(path, Application.companyName);
             }
 
-            Directory.CreateDirectory(path);
-            return path;
+            return AppendDirectory(path, Application.productName);
         }
-    }
 
-    public static string configDirectory
-    {
-        get
+        private static string AppendDirectory(string path, string dir)
         {
-            string path;
-
-            switch (Application.platform) {
-                case RuntimePlatform.WindowsEditor:
-                case RuntimePlatform.WindowsPlayer:
-                {
-                    path = Windows.GetPath("Config");
-                    break;
-                }
-                case RuntimePlatform.OSXEditor:
-                case RuntimePlatform.OSXPlayer:
-                {
-                    path = OSX.GetApplicationSupportPath("Config");
-                    break;
-                }
-                case RuntimePlatform.LinuxPlayer:
-                {
-                    path = Linux.GetConfigDirectory();
-                    break;
-                }
-                default:
-                {
-                    path = Application.persistentDataPath;
-                    break;
-                }
+            if (string.IsNullOrEmpty(dir)) {
+                return path;
             }
 
-            Directory.CreateDirectory(path);
-            return path;
+            dir = CleanForPath(dir);
+            return Path.Combine(path, dir);
         }
-    }
 
-    public static string logDirectory
-    {
-        get
+        private static string CleanForPath(string str)
         {
-            string path;
+            var invalidChars = Path.GetInvalidFileNameChars();
 
-            switch (Application.platform) {
-                case RuntimePlatform.WindowsEditor:
-                case RuntimePlatform.WindowsPlayer:
-                {
-                    path = Windows.GetPath("Logs");
-                    break;
-                }
-                case RuntimePlatform.OSXEditor:
-                case RuntimePlatform.OSXPlayer:
-                {
-                    path = OSX.GetLogsPath();
-                    break;
-                }
-                case RuntimePlatform.LinuxPlayer:
-                {
-                    path = Linux.GetLogDirectory();
-                    break;
-                }
-                default:
-                {
-                    path = Application.persistentDataPath;
-                    break;
-                }
+            foreach (var ch in invalidChars) {
+                str = str.Replace(ch, '_');
             }
 
-            Directory.CreateDirectory(path);
-            return path;
-        }
-    }
-
-    static StandardPaths()
-    {
-        includeCompanyName = false;
-    }
-
-    private static string AppendProductPath(string path)
-    {
-        if (includeCompanyName) {
-            path = AppendDirectory(path, Application.companyName);
+            return str;
         }
 
-        return AppendDirectory(path, Application.productName);
-    }
-
-    private static string AppendDirectory(string path, string dir)
-    {
-        if (string.IsNullOrEmpty(dir)) {
-            return path;
-        }
-
-        dir = CleanForPath(dir);
-        return Path.Combine(path, dir);
-    }
-
-    private static string CleanForPath(string str)
-    {
-        var invalidChars = Path.GetInvalidFileNameChars();
-
-        foreach (var ch in invalidChars) {
-            str = str.Replace(ch, '_');
-        }
-
-        return str;
-    }
-
-    private static class Windows
-    {
-        public static string GetPath(string subdirectory)
+        private static class Windows
         {
-            var result = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-                "My Games");
+            public static string GetPath(string subdirectory)
+            {
+                var result = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                    "My Games");
 
-            result = AppendProductPath(result);
-            return AppendDirectory(result, subdirectory);
-        }
-    }
-
-    private static class OSX
-    {
-        public static string GetApplicationSupportPath(string subdirectory)
-        {
-            var result = Path.Combine(
-                Environment.GetEnvironmentVariable("HOME"),
-                "Library/Application Support");
-
-            result = AppendProductPath(result);
-            return AppendDirectory(result, subdirectory);
+                result = AppendProductPath(result);
+                return AppendDirectory(result, subdirectory);
+            }
         }
 
-        public static string GetLogsPath()
+        private static class OSX
         {
-            var result = Path.Combine(
-                Environment.GetEnvironmentVariable("HOME"),
-                "Library/Logs");
+            public static string GetApplicationSupportPath(string subdirectory)
+            {
+                var result = Path.Combine(
+                    Environment.GetEnvironmentVariable("HOME"),
+                    "Library/Application Support");
 
-            return AppendProductPath(result);
-        }
-    }
-
-    private static class Linux
-    {
-        public static string GetSaveDirectory()
-        {
-            string result = Environment.GetEnvironmentVariable("XDG_DATA_HOME");
-            if (string.IsNullOrEmpty(result)) {
-                string home = Environment.GetEnvironmentVariable("HOME");
-                result = Path.Combine(home, ".local/share");
+                result = AppendProductPath(result);
+                return AppendDirectory(result, subdirectory);
             }
 
-            return AppendProductPath(result);
+            public static string GetLogsPath()
+            {
+                var result = Path.Combine(
+                    Environment.GetEnvironmentVariable("HOME"),
+                    "Library/Logs");
+
+                return AppendProductPath(result);
+            }
         }
 
-        public static string GetConfigDirectory()
+        private static class Linux
         {
-            string result = Environment.GetEnvironmentVariable("XDG_CONFIG_HOME");
-            if (string.IsNullOrEmpty(result)) {
-                string home = Environment.GetEnvironmentVariable("HOME");
-                result = Path.Combine(home, ".config");
+            public static string GetSaveDirectory()
+            {
+                string result = Environment.GetEnvironmentVariable("XDG_DATA_HOME");
+                if (string.IsNullOrEmpty(result)) {
+                    string home = Environment.GetEnvironmentVariable("HOME");
+                    result = Path.Combine(home, ".local/share");
+                }
+
+                return AppendProductPath(result);
             }
 
-            return AppendProductPath(result);
-        }
+            public static string GetConfigDirectory()
+            {
+                string result = Environment.GetEnvironmentVariable("XDG_CONFIG_HOME");
+                if (string.IsNullOrEmpty(result)) {
+                    string home = Environment.GetEnvironmentVariable("HOME");
+                    result = Path.Combine(home, ".config");
+                }
 
-        public static string GetLogDirectory()
-        {
-            return AppendProductPath("/var/log");
+                return AppendProductPath(result);
+            }
+
+            public static string GetLogDirectory()
+            {
+                return AppendProductPath("/var/log");
+            }
         }
     }
 }
